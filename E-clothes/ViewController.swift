fbe1ff46a15cbfe5be0a98ac8a2356dde2c32612
Vlad Borisenko <vlad.borisenko@hotmail.com>
2020-04-19 16:48:38 +0300

    Initial commit

---
+++ E-clothes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  E-clothes
+//
+//  Created by Владислав on 01.04.2020.
+//  Copyright © 2020 Владислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
